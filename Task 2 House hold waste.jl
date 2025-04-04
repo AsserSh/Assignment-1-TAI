15811354f9d4b0ce5b27ad987049324e83e71a1d
Asser Shimwoshili <143907399+AsserSh@users.noreply.github.com>
2025-04-04 22:36:41 +0200

    Load packages

--- Task 2 House hold waste.jl
+++ Task 2 House hold waste.jl
@@ -1,1 +1,13 @@
+using Pkg
+Pkg.status()
+Pkg.add(["DataFrames", "CSV", "GLM", "Statistics", "Plots"])
+using DataFrames, CSV, GLM, Statistics, Plots
 
+
+# Load the dataset
+file_path = "C:\\Users\\Asser\\Downloads\\global_food_wastage_dataset.csv"
+df = CSV.read(file_path, DataFrame)
+
+
+# Filter for Dairy Products only
+dairy_df = filter(row -> row."Food Category" == "Dairy Products", df)
