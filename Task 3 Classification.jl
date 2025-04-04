81cbe0a20d7341b6ee14c95d4e6c2b6ec1a66f0b
Asser Shimwoshili <143907399+AsserSh@users.noreply.github.com>
2025-04-04 22:39:44 +0200

    load packages and read data

--- Task 3 Classification.jl
+++ Task 3 Classification.jl
@@ -1,1 +1,15 @@
+# Load required packages
+using Pkg
+Pkg.add(["DataFrames", "CSV", "MLJ", "StatsPlots", "Plots", "Random", "Imbalance", "CategoricalArrays", "DecisionTree", "MLJLinearModels", "MLJFlux"])
+using DataFrames, CSV, MLJ, StatsPlots, Plots, Random, Imbalance, CategoricalArrays, DecisionTree,MLJLinearModels, MLJFlux
+
+Pkg.add("MLJDecisionTreeInterface")  # For RandomForestClassifier
+Pkg.add("XGBoost")                   # For XGBoostClassifier
+Pkg.add("MLJLinearModels")           # For LogisticClassifier
+Pkg.add("MLJFlux") 
+Pkg.add("MLJXGBoostInterface")
+
+# Load the data
+data_path = "C:\\Users\\Asser\\Downloads\\fetal_health.csv"
+df = CSV.read(data_path, DataFrame)
 
